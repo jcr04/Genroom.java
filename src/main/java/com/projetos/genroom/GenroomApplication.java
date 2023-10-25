1f881a3cd618142e9a6f504f3ce45ef38c93a725
jcr04 <joaocarlosreboucas15@gmail.com>
2023-10-25 19:23:15 -0300

    add sistem

---
+++ src/main/java/com/projetos/genroom/GenroomApplication.java
@@ -0,0 +1,13 @@
+package com.projetos.genroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetos/genroom/GenroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.genroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
